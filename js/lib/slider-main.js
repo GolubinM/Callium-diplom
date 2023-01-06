8e198e75ee86e65b73c6b00044e30421a71909bc
Mihail Golubin <golm2020@gmail.com>
2023-01-06 15:11:02 +0300

    old event listener corrected

--- js/lib/slider-main.js
+++ js/lib/slider-main.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
-function mainSlider () {
-  let mySwiper = '';
-  let breakpoint = window.matchMedia('(max-width: 767px)');
+function mainSlider() {
+  let mySwiper = "";
+  let breakpoint = window.matchMedia("(max-width: 767px)");
+  
   let breakpointChecker = function () {
     if (breakpoint.matches) {
       if (mySwiper) {
         mySwiper.destroy(true, true);
       }
 
-      mySwiper = new Swiper('.main-screen__slider', {
+      mySwiper = new Swiper(".main-screen__slider", {
         spaceBetween: 10,
         pagination: {
-          el: '.main-screen__slider-pagination',
-          type: 'fraction',
+          el: ".main-screen__slider-pagination",
+          type: "fraction",
         },
 
         scrollbar: {
-          el: '.main-screen__slider-scrollbar',
+          el: ".main-screen__slider-scrollbar",
         },
       });
       return;
@@ -26,20 +27,19 @@ function mainSlider () {
         mySwiper.destroy(true, true);
       }
 
-      mySwiper = new Swiper('.main-screen__slider', {
+      mySwiper = new Swiper(".main-screen__slider", {
         pagination: {
-          el: '.main-screen__slider-pagination',
-          type: 'bullets',
+          el: ".main-screen__slider-pagination",
+          type: "bullets",
           clickable: true,
           renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
+            return '<span class="' + className + '">' + "0" + (index + 1) + "</span>";
           },
         },
       });
     }
   };
-  breakpoint.addListener(breakpointChecker);
-  breakpointChecker();
-};
+  window.addEventListener("resize", breakpointChecker);
+}
 
 mainSlider();
