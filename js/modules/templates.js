561a06138b9cf06a13c10018a70ac542c1a2b4ef
Mihail Golubin <golm2020@gmail.com>
2023-01-16 22:07:18 +0300

    Вынос шаблонов из index.js в отдельный файл

---
+++ js/modules/templates.js
@@ -0,0 +1,25 @@
+// Шаблон для reviews блоков
+const templateReviews = (key, data) => {
+  return `<div class="swiper-slide review-item">
+        <img class="avatar-img" src="${data[key]["image"]}" width="80" height="80" alt="reviewer photo">
+        <p class="reviewer-name">${data[key]["name"]}</p>
+        <p class="reviewer-status">${data[key]["status"]}</p>
+        <p class=="review">${data[key]["review"]}</p>
+        </div>`;
+};
+
+// Шаблон для LastChance блоков
+const templateLastChance = (key, data) => {
+  return `<div class="swiper-slide box-to-buy">
+        <a class="chance-item" href="#">
+        <p class="chance-item__name">${data[key]["name"]}</p>
+        <div class="prices">
+        <p class="new-item-price">${data[key]["cost"]}</p>
+        <p class="old-item-price">${data[key]["oldCost"]}</p>
+        </div>
+        <img class="chance-img" src=${data[key]["image"]} width="204" height="300" alt="img chance">
+        </a>
+        </div>`;
+};
+//------------------------------------------------------------------------
+export { templateReviews, templateLastChance };
